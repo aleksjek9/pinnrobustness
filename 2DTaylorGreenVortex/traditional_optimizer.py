a807583742fd5545dc927604f19f2f21fc1fb4a5
aleksjek9 <47630780+aleksjek9@users.noreply.github.com>
2025-04-22 11:48:14 +0200

    Update traditional_optimizer.py

--- 2DTaylorGreenVortex/traditional_optimizer.py
+++ 2DTaylorGreenVortex/traditional_optimizer.py
@@ -58,7 +58,7 @@ class optimizer:
     def run(self):
         #Solves the inverse problem
 
-        options = {"ftol": 1e-16, "maxiter": 1, "disp": True}
+        options = {"ftol": 1e-16, "maxiter": 100, "disp": True}
 
         result = minimize(
             fun=self.error,
