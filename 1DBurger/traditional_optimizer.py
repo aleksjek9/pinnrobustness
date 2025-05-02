b3d2dfcb76f135d7a39d680c8caa632c13d6b8e4
aleksjek9 <47630780+aleksjek9@users.noreply.github.com>
2025-05-02 11:51:38 +0200

    Update traditional_optimizer.py

--- 1DBurger/traditional_optimizer.py
+++ 1DBurger/traditional_optimizer.py
@@ -58,8 +58,8 @@ class optimizer:
         #Returns test set error
 
         result = self.burgers_1d(self.viscosity, self.initial_condition)
-        y_test = np.array(self.y_test).reshape(25344)
-        result = np.array(result).reshape(25344)
+        y_test = np.array(self.y_test).reshape(25332)
+        result = np.array(result).reshape(25332)
         
         rmse = root_mean_squared_error(y_test, result)
 
