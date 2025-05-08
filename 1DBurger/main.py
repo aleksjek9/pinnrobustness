37420b5448229b5e00374c0f94b8c5a62feaac09
aleksjek9 <47630780+aleksjek9@users.noreply.github.com>
2025-05-08 16:12:13 +0200

    Update main.py

--- 1DBurger/main.py
+++ 1DBurger/main.py
@@ -3,14 +3,24 @@ from pinn import PINN_experiment
 from graphing import graph_data
 from traditional import traditional_experiment
 
-#Get the data for the experiments
-data = get_data()
-noise = [0, 0.5, 1, 2, 3, 5, 7, 10, 25]
-results = []
 
-#Run the PINN and baseline experiments
-results.append(PINN_experiment(data, noise))
-results.append(traditional_experiment(data, noise))
+def main():
+    """
+    Get the data for the experiments and define the
+    noise levels for the PINN experiment.
+    """
+    
+    data = get_data()
+    noise_levels = [0, 0.5, 1, 2, 3, 5, 7, 10, 25]
+    results = []
 
-#Graph the results
-graph_data(results)
+    # Run the PINN and baseline experiments
+    results.append(PINN_experiment(data, noise_levels))
+    results.append(traditional_experiment(data, noise_levels))
+
+    # Graph the results
+    graph_data(results)
+
+
+if __name__ == "__main__":
+    main()
