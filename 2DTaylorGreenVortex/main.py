ab2a76ecbe855ae337dcf2857081b2c3e35eaf53
aleksjek9 <47630780+aleksjek9@users.noreply.github.com>
2025-05-09 10:50:55 +0200

    Update main.py

--- 2DTaylorGreenVortex/main.py
+++ 2DTaylorGreenVortex/main.py
@@ -1,16 +1,26 @@
 from data import get_data
 from pinn import PINN_experiment
-from traditional import traditional_experiment
 from graphing import graph_data
+from traditional import traditional_experiment
 
-#Get the data for the experiments
-data = get_data()
-noise = [0, 0.001, 0.01, 0.05, 0.1, 0.25, 0.5, 1]
-results = []
 
-#Run the PINN and baseline experiments
-results.append(PINN_experiment(data, noise))
-results.append(traditional_experiment(data, noise))
+def main():
+    """
+    Get the data for the experiments and define the
+    noise levels for the PINN experiment.
 
-#Graph the results
-graph_data(results)
+    Run the PINN and baseline experiments and graph the results.
+    """
+    
+    data = get_data()
+    noise_levels = [0, 0.001, 0.01, 0.05, 0.1, 0.25, 0.5, 1]
+    results = []
+
+    results.append(PINN_experiment(data, noise_levels))
+    results.append(traditional_experiment(data, noise_levels))
+
+    graph_data(results)
+
+
+if __name__ == "__main__":
+    main()
