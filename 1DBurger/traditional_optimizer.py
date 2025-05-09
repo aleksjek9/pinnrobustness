882aef6e62e15c91a6f23112c305cff74ef64efb
aleksjek9 <47630780+aleksjek9@users.noreply.github.com>
2025-05-09 07:48:48 +0200

    Update traditional_optimizer.py

--- 1DBurger/traditional_optimizer.py
+++ 1DBurger/traditional_optimizer.py
@@ -4,11 +4,15 @@ from fem import burgers_1d, get_gradient, get_prediction_indexes
 from sklearn.metrics import root_mean_squared_error
 
 
-class optimizer:
-    #Algorithm which solves inverse problem using FEM
+class Optimizer:
+    """
+    Algorithm which solves inverse problem using FEM.
+    """
 
-    def __init__(self, data, indexes, initial_condition, burgers_1d=burgers_1d, get_gradient=get_gradient):
-        #Initializes optimizer
+
+    def __init__(self, data, indexes, initial_condition, 
+                burgers_1d=burgers_1d, get_gradient=get_gradient):
+        # Initializes optimizer
 
         self.gradient = 0
         self.y_test = data[0]
@@ -21,57 +25,65 @@ class optimizer:
         self.burgers_1d = burgers_1d
         self.get_gradient = get_gradient
 
+
     def grad(self, viscosity):
-        #Returns the gradient
+        """Returns the gradient for optimization."""
 
         print(viscosity)
-
         return np.array(self.gradient)
 
+
     def error(self, viscosity):
-        #Returns the error on training set
+        """Solves 1D Burgers. Then calculates and returns the error on training set."""
 
         try:
-            #Solves 1D Burgers
-            results, control_variable, solution_comparison, map, domain = self.burgers_1d(viscosity, self.initial_condition, gradient_mode=True)
+            results, control_variable, solution_comparison, map, domain = self.burgers_1d(
+                viscosity, self.initial_condition, gradient_mode=True
+            )
         except:
             return np.nan
 
-        #Calculates the error
-        error, self.gradient = self.get_gradient(self.l2_lambda, self.indexes, results, self.y_train, control_variable, solution_comparison, map)
-
+        error, self.gradient = self.get_gradient(
+            self.l2_lambda, 
+            self.indexes, 
+            results, 
+            self.y_train, 
+            control_variable, 
+            solution_comparison, 
+            map
+        )
         return error
 
+
     def validation(self):
-        #Returns validation set error
+        """Returns validation set error."""
 
         result = self.burgers_1d(self.viscosity, self.initial_condition)
         y_val = np.array(self.y_val).reshape(2, 256)
         result = get_prediction_indexes(result, self.val_indexes)
-
         rmse = root_mean_squared_error(y_val, result)
-
         return rmse
 
 
     def test(self):
-        #Returns test set error
+        """Returns test set error."""
 
         inds = self.indexes + self.val_indexes
         result = self.burgers_1d(self.viscosity, self.initial_condition, excluded_indices=inds)
-        y_test = np.array(self.y_test).reshape(22272)
-        result = np.array(result).reshape(22272)
-        
+        y_test = np.array(self.y_test).reshape(-1)
+        result = np.array(result).reshape(-1)
         rmse = root_mean_squared_error(y_test, result)
-
         return rmse
 
 
     def run(self):
-        #Solves the inverse problem
+        """Solves the inverse problem using Sequential Least Squares Programming optimizer."""
 
-        options = {"ftol": 1e-16, "maxiter": 100}
-
+        options = {
+            "ftol": 1e-16, 
+            "maxiter": 100
+        }
+        
         result = minimize(
             fun=self.error,
             x0=[5],
@@ -81,7 +93,4 @@ class optimizer:
             callback=None,
             options = options,
         )
-
-        result["x"]
-
         return result
