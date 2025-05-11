d681d96aaeb7910e7add506d772c88236249ade6
aleksjek9 <47630780+aleksjek9@users.noreply.github.com>
2025-05-11 08:13:20 +0200

    Update pinn.py

--- 2DTaylorGreenVortex/pinn.py
+++ 2DTaylorGreenVortex/pinn.py
@@ -17,7 +17,7 @@ torch.cuda.manual_seed_all(seed)
 The amount of times to run each experiment
 in order to get a standard deviation.
 """
-samples = 30
+samples = 5
 device = "cuda"
 
 def PINN_experiment(data, noise, verbose=True, rerun=False):
