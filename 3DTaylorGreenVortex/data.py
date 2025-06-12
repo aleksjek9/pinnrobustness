3c22cb5ad1134c787fe73c78016ded60da94758f
aleksjek9 <aleksandrajekic@outlook.com>
2025-06-12 11:59:15 +0200

    fixed 3D data.py

--- 3DTaylorGreenVortex/data.py
+++ 3DTaylorGreenVortex/data.py
@@ -1,19 +1,15 @@
 import numpy as np
-import pickle, random, torch
+import pickle
+import random
+import torch
 
 def prepare_tensor(data):
     '''Makes tensors out of lists.'''
     
-    tensor_data = []
-    
     if len(data) == 1:
         return torch.tensor(data)
-
-    for entry in data:
-        tensor_data.append(torch.tensor(entry))
-        
-    return tensor_data
-
+    return [torch.tensor(entry) for entry in data]
+    
 def get_data():
     '''Retrieves data from existing file and prepares for testing.'''
 
@@ -27,47 +23,37 @@ def get_data():
     y_test = np.array(y_test)
 
     #Training and validation indices
-    random_indices = random.sample(range(11)[1:], 3)
-    random_indices1 = random.sample(range(11)[1:], 1)
+    random_indices = random.sample(range(len(x_test)), 400000)
+    random_indices1 = random_indices[300000:]
+    random_indices = random_indices[:300000]
     random_indices.sort()
     random_indices1.sort()
 
     #Create training, physics and validation sets
-    x_train, y_train, x_val, y_val, x_all, y_all = [], [], [], [], [], []
+    x_train, y_train, x_val, y_val = [], [], [], []
 
-    for ind in random_indices:
-        #Gets time slices that are multiples of t=0.25 within domain
-        v = np.where(np.all(x_test < (2*np.pi), axis=1) & (x_test[:, 3] == ind * 0.25))[0]
-        x_train.extend(x_test[v])
-        y_train.extend(y_test[v])
+    for x in random_indices:
+        x_train.extend(x_test[x])
+        y_train.extend(y_test[x])
 
-    for ind in random_indices1:
-        #Gets time slices that are multiples of t=0.25 within domain
-        v = np.where(np.all(x_test < (2*np.pi), axis=1) & (x_test[:, 3] == ind * 0.25))[0]
-        x_val.extend(x_test[v])
-        y_val.extend(y_test[v])
+    for x in random_indices1:
+        x_val.extend(x_test[x])
+        y_val.extend(y_test[x])
 
-    for ind in np.arange(1, 11):
-        #Gets time slices that are multiples of t=0.25 within domain
-        v = np.where(np.all(x_test < (2*np.pi), axis=1) & (x_test[:, 3] == ind * 0.25))[0]
-        x_all.extend(x_test[v])
-        y_all.extend(y_test[v])
+    for x in sorted(random_indices + random_indices1, reverse=True):
+        del x_test[x]
+        del y_test[x]
    
     #Physics data is same as training data
     pde_x = x_train
 
     #Collect dataset x_train, y_train, x_val, y_val, x_test, y_test, pde_x
     data = (np.array(x_train), np.array(y_train), np.array(x_val),
-            np.array(y_val), np.array(x_all), np.array(y_all), np.array(pde_x))
+            np.array(y_val), np.array(x_test), np.array(y_test), np.array(pde_x))
 
     return data
 
 def add_noise(data, noise_level=0):
-    '''Adds noise to existing data.'''
+    """Adds noise to data."""
 
-    noisy_data = []
-
-    for entry in data:
-        noisy_data.append(entry + np.random.normal(0, noise_level, entry.shape))
-
-    return noisy_data
+    return [entry + np.random.normal(0, noise_level, entry.shape) for entry in data]
