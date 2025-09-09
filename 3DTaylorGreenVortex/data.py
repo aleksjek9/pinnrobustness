2f3b5c73fd1c05288664724fb2db57a48a8e4732
aleksjek9 <47630780+aleksjek9@users.noreply.github.com>
2025-09-09 17:14:39 +0200

    Update data.py

--- 3DTaylorGreenVortex/data.py
+++ 3DTaylorGreenVortex/data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import os
 import random
 import torch
 import secrets
@@ -11,8 +12,46 @@ def prepare_tensor(data):
         return torch.tensor(data)
     return [torch.tensor(entry) for entry in data]
     
+def create_ic():
+    '''Randomly samples observations from initial condition.'''
+
+    input, output = [], []
+    
+    for _ in range(0, 16000):
+        x, y, z, t = np.random.uniform(-np.pi, np.pi), np.random.uniform(-np.pi, np.pi), np.random.uniform(0, 2*np.pi), 0
+        input.append([x, y, z, t])
+        
+        u = (
+                np.sin(x) * np.cos(y) * np.cos(z)
+            )
+
+        v = (
+            -1 * np.cos(x) * np.sin(y) * np.cos(z) 
+        )
+        
+        w = 0
+
+        p = 0
+
+        output.append([u, v, w, p])
+        
+    return [input, output]
+    
+def create_bc():
+    '''Loads boundary condition data from generated files.'''
+
+    x_bc = list(np.load("./data/x_bc.pkl", allow_pickle=True))
+    y_bc = list(np.load("./data/y_bc.pkl", allow_pickle=True))
+    
+    return [x_bc, y_bc]
+    
 def get_data():
     '''Retrieves data from existing file and prepares for testing.'''
+    
+    if os.path.isfile("./data/all_data.npy"):
+        print("Loaded data.")
+        all_data = np.load("./data/all_data.npy", allow_pickle=True)
+        return all_data
 
     with open('data/x_test.pkl', 'rb') as f:
         x_test = pickle.load(f)
@@ -49,10 +88,15 @@ def get_data():
    
     #Physics data is same as training data
     pde_x = x_train
+    
+    ic = create_ic()
+    bc = create_bc()
 
-    #Collect dataset x_train, y_train, x_val, y_val, x_test, y_test, pde_x
+    #Collect dataset x_train, y_train, x_val, y_val, x_test, y_test, pde_x, ic, bc
     data = np.array([np.array(x_train), np.array(y_train), np.array(x_val),
-            np.array(y_val), np.array(x_test), np.array(y_test), np.array(pde_x)], dtype=object)
+            np.array(y_val), np.array(x_test), np.array(y_test), np.array(pde_x), np.array(ic), np.array(bc)], dtype=object)
+            
+    np.save("./data/all_data.npy", data)
 
     return data
 
